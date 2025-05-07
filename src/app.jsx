5da8c2248f83f217a07f28632b500244b2460d68
Sia Billal Quba <siabillal@gmail.com>
2025-05-08 00:35:59 +0700

    chore: edit button label

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,9 @@ export function App() {
   return (
     <div>
       <h1>Learn React JavaScript</h1>
-      <Button></Button>
+      <Button />
+      <Button />
+      <Button />
     </div>
   );
 }
--- src/button.jsx
+++ src/button.jsx
@@ -1,3 +1,3 @@
 export function Button() {
-  return <button>Example</button>;
+  return <button>Click me</button>;
 }
